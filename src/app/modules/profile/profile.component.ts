a0c804a9831c0d112cbb2789ee6b6922074ea673
cristian <c71917455@gmail.com>
2021-01-12 12:26:29 -0500

    windows

--- src/app/modules/profile/profile.component.ts
+++ src/app/modules/profile/profile.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/core/authentication/authentication.service';
+import { IPerson } from '../../core/interfaces/auth.interface';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  person: IPerson = {
+    names: '',
+    email: '',
+    photo: '',
+    sexo: true,
+    surnames: '',
+  };
+  constructor(private authServices: AuthenticationService) {}
 
   ngOnInit(): void {
+    this.getAuthPerson();
   }
 
+  getAuthPerson() {
+    this.authServices.getUserLogged().subscribe(
+      (data) => (this.person = data),
+      (error) => console.error(error)
+    );
+  }
 }
